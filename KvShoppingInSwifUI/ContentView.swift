bcb7b578fd356e0909324e9bd2eabc500e0b5687
Kevin Taniguchi <kv.taniguchi@icloud.com>
2020-08-01 22:40:46 -0500

    Initial Commit

---
+++ KvShoppingInSwifUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KvShoppingInSwifUI
+//
+//  Created by Kevin Taniguchi on 8/1/20.
+//  Copyright © 2020 Kevin Taniguchi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KvShoppingInSwifUITests/KvShoppingInSwifUITests.swift
@@ -0,0 +1,34 @@
+//
+//  KvShoppingInSwifUITests.swift
+//  KvShoppingInSwifUITests
+//
+//  Created by Kevin Taniguchi on 8/1/20.
+//  Copyright © 2020 Kevin Taniguchi. All rights reserved.
+//
+
+import XCTest
+@testable import KvShoppingInSwifUI
+
+class KvShoppingInSwifUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
