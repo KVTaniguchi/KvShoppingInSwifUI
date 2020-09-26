6dbc886b975a38b1a04916e2c0b3a504dfc41268
Kevin Taniguchi <kv.taniguchi@icloud.com>
2020-09-25 23:09:24 -0500

    use app main

---
+++ KvShoppingInSwifUI/AppDelegates/AppMain.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  KvShoppingInSwifUI
+//
+//  Created by Kevin Taniguchi on 8/1/20.
+//  Copyright © 2020 Kevin Taniguchi. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct KvShoppingInSwiftUI: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
