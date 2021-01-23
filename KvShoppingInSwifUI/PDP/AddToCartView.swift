c39401c4580b842f136a7bc1c3c614ba17cbd9a1
Kevin Taniguchi <kv.taniguchi@icloud.com>
2021-01-23 00:06:56 -0600

    start playing with animations

--- KvShoppingInSwifUI/PDP/AddToCartView.swift
+++ KvShoppingInSwifUI/PDP/AddToCartView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 
 struct AddToCartView: View {
     var quantity: Int
+    @State private var shouldShow = true
     
     var body: some View {
-        Button("Add To Cart") {
-            print(quantity)
+        HStack {
+            if shouldShow {
+                Text("Added ").transition(.opacity)
+            }
+            Button("Add To Cart") {
+                withAnimation(.easeInOut(duration: 3.0)) {
+                    self.shouldShow.toggle()
+                }
+            }
         }
     }
 }
